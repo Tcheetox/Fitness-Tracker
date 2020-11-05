f275dea61ffe7e2c9ebdfc4c39cca65f2fa8bcaf
KRE <tcheetox@gmail.com>
2020-11-05 10:11:14 +0100

    Added Bootstrap 4.5, created _header

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,4 @@ Rails.application.routes.draw do
   devise_for :users
 
   resources :activities
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
