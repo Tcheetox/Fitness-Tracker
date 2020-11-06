c4988d1121fa1b583bcd4b4e9ba0daa7e4f0be73
KRE <tcheetox@gmail.com>
2020-11-06 11:03:25 +0100

    Added Devise controllers, making progress on 'edit profile view'

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   root to: 'site#home'
   get 'site/home'
 
-  devise_for :users
+  # devise_for :users
+  devise_for :users , controllers: { registrations: 'users/registrations' }
 
   resources :activities
 end
