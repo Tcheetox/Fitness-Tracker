b75df3639c218e4a84d3b52d15aa9a64a3c28d17
KRE <tcheetox@gmail.com>
2020-11-16 13:57:01 +0100

    Created activity SHOW PAGE

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -7,9 +7,6 @@ class ActivitiesController < ApplicationController
   def index
     @all_activities = Activity.all.where(user: current_user)
 
-    #logger.info ">>>>>>"
-    #logger.info since
-
     if sort_column == 'kcal'
       @activities =
         @all_activities.where(name: filter_by, date: since).sort_by do |a|
@@ -24,7 +21,10 @@ class ActivitiesController < ApplicationController
   end
 
   # GET /activities/1
-  def show; end
+  def show
+    @comparable_activities = Activity.all.where(user: current_user, name: @activity.name, date: -Float::INFINITY..@activity.date.to_datetime)
+    @previous_activity = @comparable_activities.where.not(id: @activity.id).last
+  end
 
   # GET /activities/new
   def new
@@ -32,12 +32,16 @@ class ActivitiesController < ApplicationController
   end
 
   # GET /activities/1/edit
-  def edit; end
+  def edit;end
 
   # POST /activities
   def create
     @user = User.find(current_user.id)
     @activity = @user.activities.create(activity_params)
+
+    unless @activity.date.blank? # We can omit that for now, the form will throw an error anyway!
+      @activity.date = DateTime.new(@activity.date.year, @activity.date.month, @activity.date.day, Time.now.hour, Time.now.min, Time.now.sec, Time.now.zone)
+    end
 
     respond_to do |format|
       if @activity.save
@@ -57,7 +61,10 @@ class ActivitiesController < ApplicationController
   # PATCH/PUT /activities/1
   def update
     respond_to do |format|
-      if @activity.update(activity_params)
+      # Update date manipulation intended to maintain original creation time
+      p_date = activity_params[:date].to_date
+      updated_date = DateTime.new(p_date.year, p_date.month, p_date.day, @activity.date.hour, @activity.date.min, @activity.date.sec, @activity.date.zone)
+      if @activity.update(activity_params) && @activity.update_attribute(:date, updated_date)
         format.html do
           redirect_to @activity, notice: 'Activity was successfully updated.'
         end
--- app/helpers/activities_helper.rb
+++ app/helpers/activities_helper.rb
@@ -1,4 +1,37 @@
 module ActivitiesHelper
+
+  def add_chevron(dir)
+    dir == "up" ? awesome("fas fa-chevron-up") : awesome("fas fa-chevron-down")
+  end
+
+  def compare_performance(previousActivity)
+    unless @activity.distance.blank? || previousActivity.distance.blank?
+      raw("You went through " + ((@activity.distance > previousActivity.distance)? "<strong>longer</strong>" + add_chevron("up"): "<strong>shorter</strong>" + add_chevron("down")) + " distance at a " +
+      (pace > pace(previousActivity)? "<strong>faster</strong>" + add_chevron("up"): "<strong>slower</strong>" + add_chevron("down")) + " speed")
+    end
+  end
+
+  def compare_rating(previousActivity)
+    (@activity.rating > previousActivity.rating) ? raw("Better <strong>feeling</strong>") + add_chevron("up") : raw("Worse <strong>feeling</strong>") + add_chevron("down")
+  end
+
+  def compare_duration(previousActivity)
+    (@activity.duration > previousActivity.duration) ? raw("<strong>Longer</strong> workout") + add_chevron("up") : raw("<strong>Shorter</strong> workout") + add_chevron("down")
+  end
+
+  def pace(param_activity = nil)
+    activity = param_activity.nil? ? @activity : param_activity
+    activity.distance.blank? ? "N/A" : number_with_precision(@activity.distance / @activity.duration, :precision => 3)
+  end
+
+  def pace_from_params(dist, dur)
+    if dist.blank? || dur.blank?
+      0
+    else
+      number_with_precision(dist / dur, :precision => 3)
+    end
+  end
+
   def sortable(column, title = nil)
     title ||= column.titleize
     css_class = column == sort_column ? "current #{sort_direction}" : nil
