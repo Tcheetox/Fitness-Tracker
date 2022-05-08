759cfbc9462fcbaa226695be7dc225fc759d3f2e
Tcheetox <tcheetox@gmail.com>
2022-05-08 13:36:58 +0200

    attempt at ruby single worker mode

--- config/puma.rb
+++ config/puma.rb
@@ -6,10 +6,10 @@
 #
 #max_threads_count = ENV.fetch('RAILS_MAX_THREADS') { 5 }
 #min_threads_count = ENV.fetch('RAILS_MIN_THREADS') { max_threads_count }
-workers 1
-max_threads_count = 1
-min_threads_count = 1
-threads min_threads_count, max_threads_count
+#workers 1
+#max_threads_count = 1
+#min_threads_count = 1
+#threads min_threads_count, max_threads_count
 
 set_default_host '127.0.0.1'
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
@@ -28,7 +28,7 @@ pidfile ENV.fetch('PIDFILE') { 'tmp/pids/server.pid' }
 # Workers do not work on JRuby or Windows (both of which do not support
 # processes).
 #
-# workers ENV.fetch("WEB_CONCURRENCY") { 2 }
+workers ENV.fetch("WEB_CONCURRENCY") { 0 }
 
 # Use the `preload_app!` method when specifying a `workers` number.
 # This directive tells Puma to first boot the application and load code
